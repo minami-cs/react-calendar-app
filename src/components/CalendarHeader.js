35261d2ac424c2c2b1289483829eb2fe46d26956
Mina <mina9301@gmail.com>
2022-04-22 18:53:33 +0900

    Modify: CalendarHeader.js에 styled-components 적용하여 수정

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function CalendarHeader({ year, month }) {
   return (
-    <section className="calendarHeader">
-      <h1>
-        {year}년 {month}월
-      </h1>
-    </section>
+    <Header>
+      <Nav>
+        <ThisMonth>{month}</ThisMonth>
+        <ThisYear>/{year}</ThisYear>
+      </Nav>
+    </Header>
   );
 }
+
+const Header = styled.section`
+  padding-top: 10px;
+`;
+
+const Nav = styled.nav`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+`;
+
+const ThisMonth = styled.h1`
+  font-size: 2.5rem;
+  padding-left: 50px;
+`;
+
+const ThisYear = styled.h2`
+  font-size: 1.1rem;
+  padding-left: 8px;
+  line-height: 3;
+`;
