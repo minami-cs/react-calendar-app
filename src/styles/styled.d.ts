f93e344fdcc651d1eca8f24c6637ed3317954b07
Mina <mina9301@gmail.com>
2022-04-23 20:37:54 +0900

    Refactor: styles 폴더 내부 파일에 타입스크립트 적용 완료

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    notThisMonthColor: '#BFBFBF';
+    sundayColor: '#FB2D08';
+    borderColor: '#D7D7D7';
+  }
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,6 @@
-const theme = {
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
   notThisMonthColor: '#BFBFBF',
   sundayColor: '#FB2D08',
   borderColor: '#D7D7D7',
