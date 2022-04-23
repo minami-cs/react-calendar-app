e2a407dad0efcb80361b740102315456bb14e93e
Mina <mina9301@gmail.com>
2022-04-23 20:13:14 +0900

    Refactor: index.js를 index.tsx로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import Calendar from './Calendar';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <GlobalStyle />
